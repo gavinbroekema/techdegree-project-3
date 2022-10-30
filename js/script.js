a11c294b056e229c9ead2d9aacf053f098258e5d
Gavin Broekema <gavinbroekema@gmail.com>
2022-10-30 10:37:39 -0500

    Step 4: Toggle 'Other' job title input text

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-console.log('test');
+window.onload = (e) => {
+    const firstInput = document.getElementById("name");
+    firstInput.focus();
+
+    const otherRole = document.getElementById('other-job-role');
+    otherRole.style.display = 'none';
+
+}
+
+// Toggle other input textbox when 'Other' is selected
+const title = document.getElementById('title');
+title.addEventListener('change', (e) => {
+    let selectedRole = title.options[title.selectedIndex].value;
+    const otherRole = document.getElementById('other-job-role');
+    if( selectedRole == 'other') {
+        otherRole.style.display = 'inherit';
+    } else {
+        otherRole.style.display = 'none';
+    }
+})
+
+
+// 3 done
+// 4 done
